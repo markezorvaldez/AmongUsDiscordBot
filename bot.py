fdd4c9a5bf581af82e058df0899436d11d5e9c04
Mark Valdez <markezor_16@yahoo.com>
2020-10-04 21:35:24 +0100

    Add commands to mute and unmute users

--- bot.py
+++ bot.py
@@ -13,10 +13,6 @@ with open('config.json', 'r') as f:
     config = json.load(f)
 
 bot = commands.Bot(command_prefix=config['prefix'])
-    
-@bot.command(help='Prints any specified text')
-async def print(ctx, *, arg):
-    await ctx.send(arg)
 
 bot.code = None
 
@@ -27,5 +23,27 @@ async def code(ctx, arg=None):
     else:
         bot.code = arg
         await ctx.send("The code is set to %s" % bot.code)
+
+@bot.command(help='Mutes specified users')
+async def dead(ctx, members: commands.Greedy[discord.Member]):
+    await toggle_mute(members, "true")
+    await ctx.send('{} you are dead! Be quiet'.format(", ".join(x.mention for x in members)))
+
+@bot.command(help='Unmutes specified users')
+async def alive(ctx, members: commands.Greedy[discord.Member]):
+    await toggle_mute(members, "false")
+    await ctx.send('{} you are alive!'.format(", ".join(x.mention for x in members)))
+
+@bot.command(help='Mutes everyone in the voice channel')
+async def muteall(ctx):
+    await toggle_mute([await ctx.guild.fetch_member(id) for id in ctx.author.voice.channel.voice_states.keys()], "true")
         
+@bot.command(help='Unmutes everyone in the voice channel')
+async def unmuteall(ctx):
+    await toggle_mute([await ctx.guild.fetch_member(id) for id in ctx.author.voice.channel.voice_states.keys()], "false")
+
+async def toggle_mute(members, toggle):
+    for member in members:
+        await member.edit(mute=toggle,deafen=toggle)
+
 bot.run(config['token'])
