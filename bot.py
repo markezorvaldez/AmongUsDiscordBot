c72b195e228f573a58ce1a39c761c5d044012305
Mark Valdez <markezor_16@yahoo.com>
2020-10-04 14:10:17 +0100

    add logging

--- bot.py
+++ bot.py
@@ -1,13 +1,20 @@
 import json
+import logging
 import discord
 from discord.ext import commands
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 with open('config.json', 'r') as f:
     config = json.load(f)
 
 bot = commands.Bot(command_prefix=config['prefix'])
     
-@bot.command()
+@bot.command(help='Prints any specified text')
 async def print(ctx, *, arg):
     await ctx.send(arg)
 
