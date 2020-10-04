d4c95b2b310400ad16f1970930edd3b9eb60a062
Mark Valdez <markezor_16@yahoo.com>
2020-10-04 14:02:05 +0100

    add simple print command and create ignore config

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
-print("hello world")
+import json
+import discord
+from discord.ext import commands
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+bot = commands.Bot(command_prefix=config['prefix'])
+    
+@bot.command()
+async def print(ctx, *, arg):
+    await ctx.send(arg)
+
+bot.run(config['token'])
